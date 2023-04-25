a932048c33a36146db3841368fbf7c2e2d988a8e
80-p <131724872+80-p@users.noreply.github.com>
2023-04-24 20:23:51 -0400

    Add files via upload

--- switcher.lua
+++ switcher.lua
@@ -23,30 +23,3 @@ function switchCommand(bp)
         end
     end
 end
-
-        --bp.Cursor.Y = temp3 + 4
-        --bp.Cursor:Right()
-            --bp.Cursor:ResetSelection()
-        --bp.Buf:Insert(bp.Cursor.CurSelection[1], tostring(temp3))
-        --bp.Cursor:ResetSelection()
-        --bp.Cursor:Deselect(true)
-        --bp.Cursor:SelectTo(buffer.Loc(temp, temp3))
-        --bp.Cursor.CurSelection[1].X = temp
-        --bp.Cursor.CurSelection[1].Y = temp3
-        --bp.Cursor.X = bp.Cursor.X + 5
-        --bp.Cursor.CurSelection[1].X = bp.Cursor.Loc.X
-        --bp.Cursor.CurSelection[2].X = temp
-        --bp.Cursor.X = bp.Cursor.CurSelection[1].X
-        --local temp2 = {bp.Cursor.CurSelection[2].X, bp.Cursor.CurSelection[2].Y}
-        --local temp3 = temp2 - temp
-        --bp.Cursor.SetSelectionEnd(bp.Cursor, temp2)
-        --bp.Cursor.SetSelectionStart(bp.Cursor, temp)
-        --bp.Cursor.CurSelection[2].X = bp.Cursor.CurSelection[2].X - 1
-        --bp.Cursor.CurSelection[1].X = bp.Cursor.CurSelection[1].X - 5
-        --bp.Cursor.X = bp.Cursor.X + 5
-        --bp.Cursor:SetSelectionStart({5,5})
-        --bp.Cursor:SetSelectionEnd({3,5})
-        --bp.Cursor:GotoLoc(buffer.Loc(temp, temp3))
-        --bp.Cursor.OrigSelection = [buffer]
-        --bp.Cursor:GotoLoc(buffer.Loc(temp, temp3))
-        --bp.Cursor.OrigSelection[1] = buffer.Loc(temp, temp3)
